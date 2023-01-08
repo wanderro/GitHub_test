a21ca1e2592773e61a9fdc5331d7422e077559aa
wanderrro <d.papkovv@gmail.com>
2023-01-08 22:36:16 +0300

    feat: add quotient of numbers.

--- github_test/Program.cs
+++ github_test/Program.cs
@@ -10,7 +10,8 @@
             Console.WriteLine($"Выберите команду: \n" +
                               $"1. Сложить числа \n" +
                               $"2. Вычесть числа \n" +
-                              $"3. Умножить числа \n");
+                              $"3. Умножить числа \n" +
+                              $"4. Поделить числа \n");
 
             var choice = Convert.ToInt32(Console.ReadLine());
 
@@ -35,6 +36,12 @@
                 {
                     var product = firstNumber * secondNumber;
                     Console.WriteLine("Результат умножения " + product);
+                    break;
+                }
+                case 4:
+                {
+                    var quotient = firstNumber - secondNumber;
+                    Console.WriteLine("Результат деления " + quotient);
                     break;
                 }
             }
