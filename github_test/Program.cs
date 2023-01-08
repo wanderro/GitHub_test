a5cbf2099c5c3e1f0fead8d490a49aada307d01b
wanderrro <d.papkovv@gmail.com>
2023-01-08 22:15:13 +0300

    feat: add sum of numbers

--- github_test/Program.cs
+++ github_test/Program.cs
@@ -2,5 +2,13 @@
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("Введите первое число:");
+        double firstNumber = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число:");
+        double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+        double sum = firstNumber + secondNumber;
+        Console.WriteLine("Результат сложения " + sum); 
     }
 }
