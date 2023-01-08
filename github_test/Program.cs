048c30797b7d389baae768eb2d7d2dc3da4bb5f9
wanderrro <d.papkovv@gmail.com>
2023-01-08 22:17:48 +0300

    refactor: replace variables to var.

--- github_test/Program.cs
+++ github_test/Program.cs
@@ -3,12 +3,12 @@
     static void Main(string[] args)
     {
         Console.WriteLine("Введите первое число:");
-        double firstNumber = Convert.ToDouble(Console.ReadLine());
-
+        var firstNumber = Convert.ToDouble(Console.ReadLine());
+        
         Console.WriteLine("Введите второе число:");
-        double secondNumber = Convert.ToDouble(Console.ReadLine());
+        var secondNumber = Convert.ToDouble(Console.ReadLine());
 
-        double sum = firstNumber + secondNumber;
-        Console.WriteLine("Результат сложения " + sum); 
+        var sum = firstNumber + secondNumber;
+        Console.WriteLine("Результат сложения " + sum);
     }
 }
